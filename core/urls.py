83a50a1c2533f374aba23f718c073e87af05dc54
Ricardo Gaya <rrgaya@gmail.com>
2020-01-14 19:02:26 -0300

    Added chart

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, homechart, get_data
+from .views import index, get_data, ChartData
+
 
 urlpatterns = [
     path("", index),
-    path("charts/", homechart),
     path("data/", get_data),
+    path("api/chart/data/", ChartData.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+User = get_user_model()
 
 def index(request):
-    return render(request, "core/index.html", {})
-
-
-def homechart(request):
-    return render(request, "core/charts.html", {})
-
+    return render(request, "core/index.html", {"customers": 25})
 
 def get_data(request, *args, **kwargs):
     data = {
@@ -16,3 +18,20 @@ def get_data(request, *args, **kwargs):
         "customers": 10, 
     }
     return JsonResponse(data)
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+   
+    def get(self, request, format=None):
+        qs_count = User.objects.all().count()
+        labels = ['User', 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+        default_items = [qs_count, 2, 5, 2, 1, 3]
+        data = {
+            "labels": labels,
+            "default": default_items,
+            "sales": 100,
+            "customers": 10,
+            "users": User.objects.all().count()
+        }
+        return Response(data)
