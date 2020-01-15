530249a4d873555b5a8ea8548fc04988cfb78100
Ricardo Gaya <rrgaya@gmail.com>
2020-01-14 22:09:51 -0300

    Configurado diretorio de templates

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
