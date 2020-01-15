a3c86b5ac894e697016c6100f447376842b771a8
Ricardo Gaya <rrgaya@gmail.com>
2020-01-15 10:38:20 -0300

    Adicionado model passenger

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Passenger(models.Model):
+    name = models.CharField(max_length=120)
+    sex = models.CharField(max_length=10)
+    survived = models.BooleanField()
+    age = models.FloatField()
+    ticket_class = models.PositiveSmallIntegerField()
+    embarked = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Passenger"
+        verbose_name_plural = "Passengers"
